e283f6027d9d9e4c880c9180bd85d3aabfb65989
ShunTakahashi <tshun.1996@icloud.com>
2019-06-13 21:35:04 +0900

    add unique:password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :name,presence: true,length: {maximum: 30}
   validates :email,presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  before_validation {emial.dowmcase!}
+  has_secure_password
+  validates :password,presence: true,length: {minimum: 6}
+
 end
