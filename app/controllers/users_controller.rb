ccbf8cae06e4da714a59c22f6100ca14bb1d3570
ShunTakahashi <tshun.1996@icloud.com>
2019-06-15 17:35:19 +0900

    add show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id),notice:"アカウントを登録しました"
     else
       render 'new'
     end
   end
 
-
-
+  def show
+    @user = User.find(params[:id])
+  end
 
   private
 
@@ -22,6 +27,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name,:email,
                                  :password,
                                  :password_congirmation)
-
   end
 end
