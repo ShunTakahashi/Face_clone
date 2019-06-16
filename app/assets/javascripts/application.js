188999c24ef1d74ac4b5aa77b084a6afeb698c54
ShunTakahashi <tshun.1996@icloud.com>
2019-06-16 17:25:21 +0900

    Add blogs page  _form/index/new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
+
+
+$(function() {
+    var $textarea = $('#textarea');
+    var lineHeight = parseInt($textarea.css('lineHeight'));
+    $textarea.on('input', function(e) {
+        var lines = ($(this).val() + '\n').match(/\n/g).length;
+        $(this).height(lineHeight * lines);
+    });
+});
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,19 @@
 class BlogsController < ApplicationController
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(params[:content])
+    if @blog.save
+      redirect_to blogs_path,notice:"投稿が完了しました。"
+    else
+      render 'new'
+    end
+  end
 end
